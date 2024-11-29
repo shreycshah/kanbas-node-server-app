f98c051660ddb380c024465af3559c77ad172b4b
shreycshah <shreycshah@gmail.com>
2024-11-28 23:50:21 -0500

    section 3 done except update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +10,10 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollementsRoutes from './Kanbas/Enrollments/route.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING ||"mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 console.log(process.env.NETLIFY_URL)
 app.use(
